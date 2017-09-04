34b5d0c9daee185c2ede6474fd47d669c45e1a76
Aleksei Schurak <a@guava.by>
2017-09-04 19:20:53 +0300

    init

---
+++ scripts/corsed.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import corsPrefetch from 'cors-prefetch-middleware'
+import proxy from 'express-http-proxy'
+ 
+const app = express();
+ 
+app.use(corsPrefetch);
+
+app.use('/', proxy('localhost:8545'));
+
+const port = 1337;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
